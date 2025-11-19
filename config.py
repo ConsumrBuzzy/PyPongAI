7e05066a62d919b88c25835144f6871488d1489c
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-11-19 15:46:04 -0500

    refactor: Remove unused constants, colors, and path definitions from config.py.

--- config.py
+++ config.py
@@ -21,7 +21,11 @@ PADDLE_MAX_SPEED = 15
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 GRAY = (128, 128, 128)
+YELLOW = (255, 255, 0)
 
 # File Paths
 NEAT_CONFIG_PATH = "neat_config.txt"
